d23c43b8af118dd5576d497fa1ca776d7e6caf2f
EmivaldoPalacio <emivaldo.palacio@gmail.com>
2025-01-27 19:58:14 -0300

    Update semaforo.ino

--- semaforo.ino
+++ semaforo.ino
@@ -1,19 +1,19 @@
 
-#define vermelho 3
-#define amarelo 5
-#define verde 6
+#define vermelho 3    //define o pino 3 como LED vermelho
+#define amarelo 5      //define o pino 5 como LED amarelo
+#define verde 6         //define o pino 6 como LED verde 
 
 
 void setup() {
-  pinMode(vermelho, OUTPUT);
-  pinMode(amarelo, OUTPUT);
-  pinMode(verde, OUTPUT);
+  pinMode(vermelho, OUTPUT);      //define o pino como saída
+  pinMode(amarelo, OUTPUT);        //define o pino como saída
+  pinMode(verde, OUTPUT);            //define o pino como saída
 
 }
 
 void loop() {
-  digitalWrite(verde,  HIGH);
-  delay(5000);
+  digitalWrite(verde,  HIGH);      
+  delay(5000);                  
   digitalWrite(verde,  LOW);
   digitalWrite(amarelo, HIGH);
   delay(1000);
